bf019c0b7b9fc008b59326488a7230489deb9e98
nijatgarakhanov <96003352+nijatgarakhanov@users.noreply.github.com>
2022-07-18 23:51:28 +0400

    ScreenRecorder

---
+++ VideoUDP/Client/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+
+        TcpClient client = new TcpClient();
+
+        Thread t;
+
+        int size = 0;
+
+        BinaryReader br;
+        BinaryWriter bw;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            t = new Thread(() =>
+            {
+                using var UdpClient = new UdpClient(26031);
+                var remoteEP = new IPEndPoint(IPAddress.Any, 0);
+
+                List<byte> by = new List<byte>();
+
+                while (true)
+                {
+                    size = br.ReadInt32();
+                    bw.Write("next");
+                    if (size > 65000)
+                    {
+                        do
+                        {
+                            var bytes = UdpClient.Receive(ref remoteEP);
+                            by.AddRange(bytes);
+                            size -= bytes.Length;
+                        } while (size > 0);
+
+
+                        pictureBox1.Image = ByteToImage(by.ToArray());
+
+                    }
+                    else
+                    {
+
+                        var bytes = UdpClient.Receive(ref remoteEP);
+                        pictureBox1.Image = ByteToImage(bytes);
+                    }
+                    by.Clear();
+                }
+
+
+
+            });
+
+
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            t.Abort();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var ip = IPAddress.Parse(textBox1.Text);
+            var port = 27031;
+
+
+            client.Connect(ip, port);
+
+            var stream = client.GetStream();
+
+            bw = new BinaryWriter(stream);
+            br = new BinaryReader(stream);
+
+            bw.Write("success");
+            var answer = br.ReadString();
+
+            if (answer == "success")
+            {
+                t.Start();
+            }
+
+        }
+
+
+        public static Bitmap ByteToImage(byte[] blob)
+        {
+            MemoryStream mStream = new MemoryStream();
+            byte[] pData = blob;
+            mStream.Write(pData, 0, Convert.ToInt32(pData.Length));
+            Bitmap bm = new Bitmap(mStream, false);
+            mStream.Dispose();
+            return bm;
+        }
+
+    }
+}
---
+++ VideoUDP/Server/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 27031);
+
+            listener.Start(10);
+
+            while (true)
+            {
+                var client = listener.AcceptTcpClient();
+                var stream = client.GetStream();
+
+                var br = new BinaryReader(stream);
+                var bw = new BinaryWriter(stream);
+
+                var data = br.ReadString();
+
+
+                if (data == "success")
+                {
+                    bw.Write("success");
+                    Console.WriteLine(data);
+                    var UpdClient = new UdpClient();
+                    var connectEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 26031);
+
+                    while (true)
+                    {
+                        Bitmap memoryImage;
+                        memoryImage = new Bitmap(1920, 1080); //Shekil
+                        Size s = new Size(memoryImage.Width, memoryImage.Height);
+                        Graphics memoryGraphics = Graphics.FromImage(memoryImage);
+                        memoryGraphics.CopyFromScreen(0, 0, 0, 0, s);
+
+                        ImageConverter converter = new ImageConverter();
+                        var bytes = (byte[])converter.ConvertTo(memoryImage, typeof(byte[]));
+
+                        bw.Write(bytes.Length);
+                        br.ReadString();
+                        var skipCount = 0;
+                        var maxValue = ushort.MaxValue - 30;
+                        var bytesLen = bytes.Length;
+
+                        if (bytesLen > maxValue)
+                        {
+                            while (skipCount + maxValue <= bytesLen)
+                            {
+                                UpdClient.Send(bytes
+                                    .Skip(skipCount)
+                                    .Take(maxValue)
+                                    .ToArray(), maxValue, connectEP);
+                                skipCount += maxValue;
+                            }
+
+                            if (skipCount != bytesLen)
+                            {
+                                UpdClient.Client.ReceiveBufferSize = bytesLen - skipCount;
+                                UpdClient.Send(bytes.Skip(skipCount)
+                                    .Take(bytesLen - skipCount)
+                                    .ToArray(), bytesLen - skipCount, connectEP);
+                            }
+
+                        }
+                        else
+                        {
+                            UpdClient.Send(bytes, bytes.Length, connectEP);
+                        }
+                    }
+
+                }
+
+
+
+
+            }
+
+
+        }
+    }
+}
